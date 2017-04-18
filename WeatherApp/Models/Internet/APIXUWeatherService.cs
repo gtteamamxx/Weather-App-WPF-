2625151764404363dc95131dfdab6f4d757526f5
gtteamamxx <grs4_98@o2.pl>
2017-04-18 21:04:09 +0200

    Added searching city

--- WeatherApp/Models/Internet/APIXUWeatherService.cs
+++ WeatherApp/Models/Internet/APIXUWeatherService.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using APIXULib;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +12,19 @@ namespace WeatherApp.Models.Internet
 {
     class APIXUWeatherService
     {
+        public async static Task<List<Location>> GetAutoCompleteCityNamesAsync(string value)
+        {
+            string urlToCall = $"http://api.apixu.com/v1/search.json?key=6636b95c6663415b913135418160412&q={value}";
+
+            using (WebClient webClient = new WebClient())
+            {
+                using (var streamReader = new StreamReader( await webClient.OpenReadTaskAsync(
+                        new Uri(urlToCall)).ConfigureAwait(false)))
+                {
+                    string responseJson = await streamReader.ReadToEndAsync().ConfigureAwait(false);
+                    return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<List<Location>>(responseJson)).ConfigureAwait(false);
+                }
+            }
+        }
     }
 }
---
+++ WeatherApp/ViewModels/SearchCityPageViewModel.cs
@@ -0,0 +1,102 @@
+﻿using APIXULib;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+using WeatherApp.Models.Internet;
+
+namespace WeatherApp.ViewModels
+{
+    class SearchCityPageViewModel : ViewModelBase
+    {
+        public SearchCityPageViewModel()
+        {
+            CityTips = new ObservableCollection<Location>();
+        }
+
+        private bool _IsTipSelected = false;
+        #region Properties
+        private bool _IsCityTipOpen;
+        public bool IsCityTipOpen
+        {
+            get => _IsCityTipOpen;
+            set
+            {
+                if (CityTips.Count() > 0)
+                    return;
+                OnPropertyChanged(ref _IsCityTipOpen, value);
+            }
+        }
+
+        private ObservableCollection<Location> _CityTips;
+        public ObservableCollection<Location> CityTips
+        {
+            get => _CityTips;
+            set => OnPropertyChanged(ref _CityTips, value);
+        }
+
+        private string _SearchCityName;
+        public string SearchCityName
+        {
+            get => _SearchCityName;
+            set
+            {
+                if (_SearchCityName == value)
+                    return;
+                _SearchCityName = value;
+                SearchTextChanged(value);
+                OnPropertyChanged(ref _SearchCityName, value);
+            }
+        }
+
+        private Location _SelectedTipCity;
+        public Location SelectedTipCity
+        {
+            get => _SelectedTipCity;
+            set
+            {
+                if(value != null)
+                {
+                    _IsTipSelected = true;
+                    SearchCityName = value.name;
+                }
+                OnPropertyChanged(ref _SelectedTipCity, value);
+            }
+        }
+        #endregion
+
+        private void SearchTextChanged(string searchText)
+        {
+            CityTips.Clear();
+
+            if (_IsTipSelected ||  string.IsNullOrEmpty(searchText))
+            {
+                if (_IsTipSelected)
+                    _IsTipSelected = false;
+
+                IsCityTipOpen = false;
+                return;
+            }
+
+            IsCityTipOpen = true;
+
+            List<Location> suggestedLocations = Task.Run(() => APIXUWeatherService.GetAutoCompleteCityNamesAsync(searchText)).Result;
+
+            if(suggestedLocations.Count() == 0)
+            {
+                IsCityTipOpen = false;
+                return;
+            }
+
+            foreach (Location suggestedLocation in suggestedLocations)
+            {
+                CityTips.Add(suggestedLocation);
+            }
+        }
+
+    }
+}
